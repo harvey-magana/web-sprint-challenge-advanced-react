5ee081010f433b5ea57114828d2c1c30c3fdf5ff
harvey magana <heyharv2012@gmail.com>
2020-09-19 07:23:38 -0700

    Testing code with the useForm hook.

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import useForm from "../hooks/useForm";
 
 const initialValue = {
   firstName: "",
@@ -15,8 +16,9 @@ const initialValue = {
 
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+  const [values, handleChanges, handleSubmit] = useForm(initialValue);
 
+  /*
   const handleChanges = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
   };
@@ -25,7 +27,7 @@ const CheckoutForm = (props) => {
     e.preventDefault();
     setShowSuccessMessage(true);
   };
-
+  */
   return (
     <>
       <form onSubmit={handleSubmit}>
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -2,6 +2,7 @@
 import { useState } from "react";
 
 const useForm = (initialValues) => {
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
     const [values, setValues] = useState(initialValues);
 
     const handleChanges = (e) => {
@@ -10,8 +11,11 @@ const useForm = (initialValues) => {
     
     const handleSubmit = (e) => {
         e.preventDefault();
+        console.log("I was clicked...")
         setShowSuccessMessage(true);
     };
 
     return [values, handleChanges, handleSubmit];
 }
+
+export default useForm;
